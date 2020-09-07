fb3241917dfa7c5a5a0f3b877f716e9ded3e605d
Bharat <bharat@wealthface.com>
2020-09-07 19:36:46 +0400

    some code added

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-thisset = {"apple", "banana", "cherry"}
-thisset.clear()
-del thisset
-print(thisset)
+import numpy as np
+
+
+
+
+# shaping the array 
+arr = np.array([1,2])
+arr2 = np.array([2,3])
+arr3 =  np.concatenate((arr, arr2))
+print(arr3)
